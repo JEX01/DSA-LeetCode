7e5993a01f3646b3e44f0f94290b16b4098c3816
JEET PATEL <162669331+JEX01@users.noreply.github.com>
2025-10-29 05:30:28 +0530

    Time: 0 ms (100%), Space: 13.3 MB (95.63%) - LeetHub

--- 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.cpp
+++ 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.cpp
@@ -2,9 +2,8 @@ class Solution {
 public:
     bool checkIfExist(vector<int>& arr) {
         int i = 0 ;
-        int j = 1 ;
         while( i < arr.size()){
-            j = i + 1;
+            int j = i + 1;
         while( j < arr.size() ){
 
             if( arr[i] == 2 * arr[j] or 2 * arr[i] == arr[j]  ){
