868aff914a5908f4b7cda3c77fb26bdd47559f7c
JEET PATEL <162669331+JEX01@users.noreply.github.com>
2025-11-27 00:23:01 +0530

    Time: 0 ms (100%), Space: 20.6 MB (57.6%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int singleNumber(vector<int>& nums) {
         int x = 0;
-//for me:- it delete or remove the constant or duplication using XOR like = 1^2^2 
+//for me:- it delete or remove the constant or duplication using XOR like = 1^2^2 with constant space and loop
         for (int n : nums) {
             x ^= n;
         }
