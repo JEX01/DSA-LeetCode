1bfb13c76878e63456cf1c8b16b47ccc1fa5fa04
JEET PATEL <162669331+JEX01@users.noreply.github.com>
2025-11-27 00:22:34 +0530

    Time: 0 ms (100%), Space: 20.6 MB (57.6%) - LeetHub

---
+++ 0136-single-number/0136-single-number.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int x = 0;
+//for me:- it delete or remove the constant or duplication using XOR like = 1^2^2 
+        for (int n : nums) {
+            x ^= n;
+        }
+        return x;
+    }
+};
