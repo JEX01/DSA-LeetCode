b5e03d52e6b81fcf28e498386318fb30c31e887d
JEET PATEL <162669331+JEX01@users.noreply.github.com>
2025-12-01 15:52:14 +0530

    Time: 0 ms (100%), Space: 8.8 MB (37.73%) - LeetHub

---
+++ 0290-word-pattern/0290-word-pattern.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    bool wordPattern(string pattern, string s) {
+        vector<string> words;
+        string temp;
+        stringstream ss(s);
+
+        while (ss >> temp) {
+            words.push_back(temp);
+        }
+
+        if (pattern.length() != words.size()) return false;
+
+        unordered_map<char, string> p2w;
+        unordered_map<string, char> w2p;
+
+        for (int i = 0; i < pattern.length(); i++) {
+            char ch = pattern[i];
+            string word = words[i];
+
+            if (p2w.count(ch)) {
+                if (p2w[ch] != word) return false;
+            } else {
+                if (w2p.count(word)) return false;
+                p2w[ch] = word;
+                w2p[word] = ch;
+            }
+        }
+        return true;
+    }
+};
