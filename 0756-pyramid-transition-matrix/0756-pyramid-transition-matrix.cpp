ea18af943939ea12038043f68dd5ad6946454f60
JEET PATEL <162669331+JEX01@users.noreply.github.com>
2025-12-30 06:52:43 +0530

    Time: 951 ms (30.81%), Space: 12.2 MB (76.22%) - LeetHub

--- 0756-pyramid-transition-matrix/0756-pyramid-transition-matrix.cpp
+++ 0756-pyramid-transition-matrix/0756-pyramid-transition-matrix.cpp
@@ -1,40 +1,25 @@
 class Solution {
 public:
-    unordered_map<string, vector<char>> mp;
-    unordered_map<string, bool> memo;
-
-    bool dfs(int idx, string& curr, string& next) {
-        if (idx == curr.size() - 1) {
-            if (memo.count(next))
-                return memo[next];
-            return memo[next] = canBuild(next);
+    bool solve(int i, int j, vector<vector<char>> &v, unordered_map<string, vector<char>> &m){
+        if(i<=j) --i, j=0;
+        if(i==0 && j==0) return true;
+        string key = {v[i][j], v[i][j+1]};
+        bool valid=false;
+        for(auto &x: m[key]){
+            v[i-1][j] = x;
+            valid = valid || solve(i, j+1, v, m);
         }
-
-        string key = curr.substr(idx, 2);
-        if (!mp.count(key))
-            return false;
-
-        for (char ch : mp[key]) {
-            next.push_back(ch);
-            if (dfs(idx + 1, curr, next))
-                return true;
-            next.pop_back();
-        }
-        return false;
-    }
-
-    bool canBuild(string curr) {
-        if (curr.size() == 1)
-            return true;
-
-        string next;
-        return dfs(0, curr, next);
+        return valid;
     }
 
     bool pyramidTransition(string bottom, vector<string>& allowed) {
-        for (auto& s : allowed) {
-            mp[s.substr(0, 2)].push_back(s[2]);
+        unordered_map<string, vector<char>> m;
+        for(int i=0; i<allowed.size(); ++i){
+            m[{allowed[i][0], allowed[i][1]}].push_back(allowed[i][2]);
         }
-        return canBuild(bottom);
+        int n=bottom.length();
+        vector<vector<char>> v(n, vector<char>(n));
+        for(int i=0; i<n; ++i) v[n-1][i] = bottom[i];
+        return solve(n-1, 0, v, m);
     }
 };
